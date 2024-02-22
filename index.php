453fe0d2649c569954c142e19b32bb8d7b574915
2Ran076 <D290119@edu.curio.nl>
2024-02-22 10:55:21 +0100

    fix 1

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BlueEasy</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+  </head>
+  <body>
+    <header>
+        <div class="a-background">
+          <div class="wrapper">
+            <div class="nav-bar">
+              <img src="images/logos/curio-13-midden-blauw-logo-rgb.png" alt="" class="curio-logo">
+              <div class="nav-menu">
+                <nav>
+                  <a href="">HOME</a>
+                  <a href="">ROOSTER/VAKKEN</a>
+                  <a href="">DOCENTEN</a>
+                  <a href="">TRAJECT</a>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="background">
+          <div class="wrapper">
+            <div class="info-opl">
+                <h2>Informatie Opleiding</h2>
+                <h4>I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE"</h4>
+            </div>
+          </div>
+        </div>
+    </header>
+    <main>
+      <section id="rooster">
+        <div class="b-background">
+          <div class="wrapper rooster">
+            <h1><span class="letterROO">ROO</span>STERS EN VAKKEN</h1>
+            <div class="containers">
+              <div class="rooster-container">
+                <img src="img/Icon_coffee.png" alt="" />
+                <h2>COFFEE</h2>
+                <p>
+                  Apple pie icing sweet. Brownie jelly-o applicake applicake
+                  sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                  candy sweet tart brownie. Tiramisu applicake jujubes.
+                </p>
+              </div>
+              <div class="rooster-container">
+                <img src="img/Icon_instant.png" alt="" />
+                <h2>INSTANT</h2>
+                <p>
+                  Apple pie icing sweet. Brownie jelly-o applicake applicake
+                  sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                  candy sweet tart brownie. Tiramisu applicake jujubes.
+                </p>
+              </div>
+              <div class="rooster-container">
+                <img src="img/Icon_serious.png" alt="" />
+                <h2>SERIOUS</h2>
+                <p>
+                  Apple pie icing sweet. Brownie jelly-o applicake applicake
+                  sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                  candy sweet tart brownie. Tiramisu applicake jujubes.
+                </p>
+              </div>
+              <div class="rooster-container">
+                <img src="img/Icon_frame.png" alt="" />
+                <h2>FRAME</h2>
+                <p>
+                  Apple pie icing sweet. Brownie jelly-o applicake applicake
+                  sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                  candy sweet tart brownie. Tiramisu applicake jujubes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="docenten">
+          <div class="wrapper docenten">
+            <div class="head-docenten">
+              <h2><span class="letterDO">DO</span>CENTEN</h2>
+            </div>
+          </div>
+      </section>
+      <section id="traject">
+          <div class="wrapper traject">
+            <div class="head-traject">
+              <h2><span class="letterTRA">TRA</span>JECT</h2>
+            </div>
+          </div>
+      </section>
+    </main>
+    <footer></footer>
+  </body>
+</html>
